f5f60496ff45d1c2a943c57b07ec2eb15a65b2f7
bhupendra <bhupendra.shekhawat@heymath.com>
2017-03-19 20:14:30 +0530

    Initial commit

---
+++ app/src/main/java/edfora/player/bhupendra/com/edforaplayer/rest/model/Track.java
@@ -0,0 +1,13 @@
+package edfora.player.bhupendra.com.edforaplayer.rest.model;
+
+/**
+ * Created by root on 19/3/17.
+ */
+
+public class Track {
+
+    private String mSong;
+    private String mUrl;
+    private String mArtists;
+    private String mCoverImage;
+}
